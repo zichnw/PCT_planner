4d3146c08cfcc1617794abbb57c104c606bfcc22
zichnw <zichnwei@gmail.com>
2025-12-22 08:13:49 +0000

    building cfg

--- tomography/config/scene_building.py
+++ tomography/config/scene_building.py
@@ -4,7 +4,7 @@ from .scene import Scene
 class SceneBuilding(Scene):
     def __init__(self):
         super().__init__()
-        self.pcd.file_name = 'building2_9.pcd'
+        self.pcd.file_name = 'building.pcd'
         
         self.map.resolution = 0.10
         self.map.ground_h = 0.0
@@ -14,9 +14,9 @@ class SceneBuilding(Scene):
         self.trav.interval_min = 0.50
         self.trav.interval_free = 0.65
         self.trav.slope_max = 0.40
-        self.trav.step_max = 0.17
-        self.trav.standable_ratio = 0.20
+        self.trav.step_max = 0.3
+        self.trav.standable_ratio = 0.50
         self.trav.cost_barrier = 50.0
-        self.trav.safe_margin = 0.4
+        self.trav.safe_margin = 0.2
         self.trav.inflation = 0.2
 
